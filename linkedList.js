8783d709b66d284d26598103c3be8969ef49d205
Robert Albenda <ralbenda@gmail.com>
2022-05-12 12:43:02 -0400

    some updates

---
+++ linkedList.js
@@ -0,0 +1,21 @@
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.length = 0;
+  }
+
+  insertAtHead(val) {
+    const newNode = new LinkedListNode(val, this.head);
+    this.head = newNode;
+    length++;
+  }
+}
+
+class LinkedListNode {
+  constructor(value, next) {
+    this.value = value;
+    this.next = next;
+  }
+}
+
+module.exports = LinkedList;
